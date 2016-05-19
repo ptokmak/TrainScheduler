6e94a788776ffb65cbe2fc0e3151be7a3cc201c0
Pinar Tokmak <ptokmak@hotmail.com>
2016-05-19 00:18:50 -0400

    added firebase link and basic button function

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+//Link to Firebase
+var employeeData = new Firebase("https://burning-torch-3473.firebaseio.com/");
+
+//Button for adding new Schedules
+ $("#addTrainBtn").on("click", function(){
+
+
+ });
+
+
+})
