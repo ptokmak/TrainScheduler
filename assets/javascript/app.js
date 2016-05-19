8a4a603de684bd7a9c4aa90159881c6d59c8f0a1
Pinar Tokmak <ptokmak@hotmail.com>
2016-05-19 01:04:07 -0400

    Added an alert and clear text box property

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,45 @@
-$(document).ready(function() {
+
 
 //Link to Firebase
-var employeeData = new Firebase("https://burning-torch-3473.firebaseio.com/");
+var trainData = new Firebase("https://burning-torch-3473.firebaseio.com/");
 
 //Button for adding new Schedules
  $("#addTrainBtn").on("click", function(){
 
-	// Grabs user input
-	var trainName = $("#employeeNameInput").val().trim();
-	var trainDest = $("#destinationInput").val().trim();
-	var trainStart = moment($("#startInput").val().trim(), "HH:mm").format("X");
-	var trainFreq = $("#frequencyInput").val().trim();
+		// Grabs user input
+		var trainName = $("#trainNameInput").val().trim();
+		var trainDest = $("#destinationInput").val().trim();
+		var trainStart = $("#startInput").val().trim();
+		var trainFreq = $("#frequencyInput").val().trim();
 
-	//Creates local "temporary" object for holding employee data
-	var newTrain ={
-		tName: trainName,
-		tDest: trainDest,
-		tStart: trainStart,
-		tFreq: trainFreq
-	};
-	
+		//Creates local "temporary" object for holding employee data
+		var newTrain ={
+			tName: trainName,
+			tDest: trainDest,
+			tStart: trainStart,
+			tFreq: trainFreq
+		};
 
+		//Uploading entered data to database
+		trainData.push(newTrain); 
+
+		//Logging user input to console
+		console.log(newTrain.tName);
+		console.log(newTrain.tDest);
+		console.log(newTrain.tStart);
+		console.log(newTrain.tFreq);
+
+		// Alert
+		alert("New Train Schedulesuccessfully added");
+
+		// Clears all of the text-boxes
+		$("#trainNameInput").val("");
+		$("#destinationInput").val("");
+		$("#startInput").val("");
+		$("#frequencyInput").val("");
+
+		// Prevents moving to new page
+		return false;
 });
 
 
-})
