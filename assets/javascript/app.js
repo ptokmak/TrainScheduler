2ff3d2ef283ca16cbe2e5a177e8516ec44bed9ff
Pinar Tokmak <ptokmak@hotmail.com>
2016-05-19 00:30:50 -0400

    Added temporary object for user input

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,8 +6,22 @@ var employeeData = new Firebase("https://burning-torch-3473.firebaseio.com/");
 //Button for adding new Schedules
  $("#addTrainBtn").on("click", function(){
 
+	// Grabs user input
+	var trainName = $("#employeeNameInput").val().trim();
+	var trainDest = $("#destinationInput").val().trim();
+	var trainStart = moment($("#startInput").val().trim(), "HH:mm").format("X");
+	var trainFreq = $("#frequencyInput").val().trim();
 
- });
+	//Creates local "temporary" object for holding employee data
+	var newTrain ={
+		tName: trainName,
+		tDest: trainDest,
+		tStart: trainStart,
+		tFreq: trainFreq
+	};
+	
+
+});
 
 
 })
